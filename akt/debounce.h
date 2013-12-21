91f8c94189d62f0d8f49c92b6f4ee1f27b57ea96
Andy Turk <andyturk@gmail.com>
2013-12-21 13:49:10 -0800

    adding debounce class

---
+++ akt/debounce.h
@@ -0,0 +1,57 @@
+// -*- Mode:C++ -*-
+
+#pragma once
+
+/**
+ * \file       debounce.h
+ * \class      akt::Debounce
+ * \brief      Removes contact noise from a set of raw hardware switches
+ * 
+ * \details    A single instance of the Debounce class can remove contact noise
+ *             from a bank of up to 32 individual hardware switches. External
+ *             code is responsible for reading the raw switch values and placing
+ *             them into a uint32_t mask on a periodic basis (e.g., every 20 msec).
+ *             The uint32_t should have a 1 bit for each switch that is
+ *             pressed/active and a 0 bit otherwise.
+ *             Each time the update_state() method is called, the Debounce
+ *             instance recalculates the debounced state and calls
+ *             state_changed() when any of the switches have changed.
+ */
+
+namespace akt {
+  class Debounce {
+  public:
+    enum {
+      DWELL = 10 // as in "dwell time"
+    };
+
+    Debounce() {
+      next = debounced_state = 0;
+      for (unsigned i=0; i < DWELL; ++i) history[i] = 0;
+    }
+
+    void update_state(uint32_t raw_switch_values) {
+      history[next++] = raw_switch_values;
+      uint32_t new_state = 0xffffffff;
+      for (unsigned i=0; i < DWELL; ++i) new_state &= history[i];
+
+      if (new_state != debounced_state) {
+        state_changed(debounced_state, new_state);
+        debounced_state = new_state;
+      }
+    }
+    
+    uint32_t state() const {
+      return debounced_state;
+    }
+
+  protected:
+    virtual void state_changed(uint32_t old_state, uint32_t new_state) {
+    }
+
+  private:
+    uint32_t debounced_state;
+    uint32_t history[DWELL];
+    unsigned next;
+  };
+};
