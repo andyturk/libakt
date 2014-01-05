da113494a47157c36cbeeea019c5b2b0d6c0e467
Andy Turk <andyturk@gmail.com>
2014-01-05 13:28:20 -0800

    significant refactoring of command line shell shell commands are independent instances now

--- akt/shell.cc
+++ akt/shell.cc
@@ -2,258 +2,265 @@
 #include "ch.h"
 #include "hal.h"
 #include "chprintf.h"
-#include <string.h>
 
-namespace akt {
-  ShellBase::ShellBase(SerialDriver *sd,
-                       const command_entry *commands,
-                       void *work_area,
-                       size_t work_area_size,
-                       const char *name,
-                       tprio_t prio) :
-    ThreadBase(work_area, work_area_size, name, prio),
-    sd(sd),
-    tty((BaseSequentialStream *) sd),
-    prompt("> "),
-    command_table(commands == 0 ? default_command_table : commands)
-  {
+#include <cstring>
+
+using namespace akt;
+
+Ring<ShellCommand>    ShellCommand::commands __attribute__ ((init_priority(200)));
+BaseSequentialStream *ShellCommand::tty = 0;
+char                  ShellCommand::line[LINE_SIZE];
+unsigned int          ShellCommand::line_length;
+char                 *ShellCommand::argv[MAX_ARGS];
+unsigned int          ShellCommand::argc;
+const char           *ShellCommand::prompt = "> ";
+
+ShellCommand::ShellCommand(const char *n) :
+  Ring(),
+  name(n)
+{
+  join(commands);
+}
+
+ShellCommand *ShellCommand::find_command(const char *name) {
+  if (!strcmp(name, "?")) name = "help";
+
+  for (Iterator i=commands.begin(); i != commands.end(); ++i) {
+    const char *cmd_name = i->name;
+
+    if (!strcmp(name, cmd_name)) return i;
   }
 
-  void ShellBase::readline() {
-    chprintf(tty, prompt);
-    ll = 0;
+  return 0;
+}
 
-    while (ll < LINE_SIZE) {
-      char c = (char) chSequentialStreamGet(tty);
+void ShellCommand::read_line() {
+  chprintf(tty, prompt);
+  line_length = 0;
 
-      switch (c) {
-      case 0x04 : // ctrl-D
-        chprintf(tty, "^D");
-        line[ll] = 0;
-        return;
+  while (line_length < LINE_SIZE) {
+    char c = (char) chSequentialStreamGet(tty);
 
-      case 0x7f : // delete
-      case 0x08 : // ctrl-H
-        if (ll > 0) {
-          chSequentialStreamPut(tty, 0x08);
-          chSequentialStreamPut(tty, ' ');
-          chSequentialStreamPut(tty, 0x08);
-          ll--;
-        }
-        break;
+    switch (c) {
+    case 0x04 : // ctrl-D
+      chprintf(tty, "^D");
+      line[line_length] = 0;
+      return;
 
-      case 0x15 : // ctrl-U
-        chprintf(tty, "^U\r\n%s", prompt);
-        line[ll=0] = 0;
-        break;
-
-      case '\r' :
-        chprintf(tty, "\r\n");
-        line[ll] = 0;
-        return;
-
-      default :
-        if (c < ' ' || c >= 0x80) {
-          //chprintf(tty, "<0x%02x>", c);
-          continue;
-        }
-
-        if (ll >= LINE_SIZE-1) continue;
-        chSequentialStreamPut(tty, c);
-        line[ll++] = c;
+    case 0x7f : // delete
+    case 0x08 : // ctrl-H
+      if (line_length > 0) {
+        chSequentialStreamPut(tty, 0x08);
+        chSequentialStreamPut(tty, ' ');
+        chSequentialStreamPut(tty, 0x08);
+        line_length--;
       }
+      break;
+
+    case 0x15 : // ctrl-U
+      chprintf(tty, "^U\r\n%s", prompt);
+      line[line_length=0] = 0;
+      break;
+
+    case '\r' :
+      chprintf(tty, "\r\n");
+      line[line_length] = 0;
+      return;
+
+    default :
+      if (c < ' ' || c >= 0x80) {
+        //chprintf(tty, "<0x%02x>", c);
+        continue;
+      }
+
+      if (line_length >= LINE_SIZE-1) continue;
+      chSequentialStreamPut(tty, c);
+      line[line_length++] = c;
     }
   }
+}
 
-  void ShellBase::puts(const char *msg) {
-    chprintf(tty, "%s\r\n", msg);
+void ShellCommand::parse_line() {
+  unsigned int i=0;
+  argc = 0;
+
+  while (i < line_length && argc < MAX_ARGS) {
+    // start argument word
+    argv[argc++] = &line[i];
+    while (i < line_length && line[i] != ' ') i++;
+    line[i++] = 0;
+
+    // skip whitespace
+    while (i < line_length && line[i] == ' ') i++;
   }
+}
 
-  void ShellBase::parseline() {
-    unsigned int i=0;
-    argc = 0;
+msg_t ShellCommand::run_loop(void *) {
+  chThdSelf()->p_name = "shell";
 
-    while (i < ll && argc < MAX_ARGS) {
-      // start argument word
-      argv[argc++] = &line[i];
-      while (i < ll && line[i] != ' ') i++;
-      line[i++] = 0;
+  while (tty != 0) {
+    read_line();
+    parse_line();
 
-      // skip whitespace
-      while (i < ll && line[i] == ' ') i++;
-    }
-  }
+    if (argc > 0) {
+      ShellCommand *cmd = find_command(argv[0]);
 
-  const ShellBase::command_entry *ShellBase::find(const char *name) {
-    if (!strcmp(name, "?")) name = "help";
-
-    for (unsigned i=0; command_table; ++i) {
-      if (!command_table[i].name) return 0;
-      if (!strcmp(name, command_table[i].name)) return command_table + i;
-    }
-
-    __builtin_unreachable();
-  }
-
-  const ShellBase::command_entry ShellBase::default_command_table[] = {
-    {&help, "help"},
-    {&info, "info"},
-#if CH_USE_REGISTRY
-    {&threads, "threads"},
-#endif
-    {&memory, "memory"},
-    {&reset, "reset"},
-    {0, 0}
-  };
-
-  void ShellBase::list_commands() {
-    for (unsigned i=0; command_table; ++i) {
-      const command_entry &ce = command_table[i];
-      if (ce.name) {
-        ce.fn(this, 0, 0);
-        // chprintf(tty, " %s\r\n", ce.name);
+      if (cmd) {
+        cmd->exec(argc, argv);
       } else {
-        break;
+        chprintf(tty, "%s ?\r\n", argv[0]);
       }
     }
   }
 
-  msg_t ShellBase::run() {
-    while (1) {
-      readline();
-      parseline();
+  return 0;
+}
 
-      if (argc > 0) {
-        const command_entry *c = find(argv[0]);
+HelpCommand::HelpCommand() :
+  ShellCommand("help")
+{
+}
 
-        if (c) {
-          (c->fn)(this, argc, argv);
-        } else {
-          chprintf(tty, "%s ?\r\n", argv[0]);
-        }
-      }
-    }
-
-    return RDY_OK;
+void HelpCommand::exec(int argc, char *argv[]) {
+  if (argc == 0) {
+    chprintf(tty, "help     -- list available commands\r\n");
+  } else {
+    for (Iterator i=commands.begin(); i != commands.end(); ++i) i->exec(0, 0);
   }
+}
 
-  void ShellBase::help(int argc, char *argv[]) {
-    if (argc == 0) {
-      chprintf(tty, "help     -- list available commands\r\n");
-    } else {
-      list_commands();
-    }
-  }
+InfoCommand::InfoCommand() :
+  ShellCommand("info")
+{
+}
 
-  void ShellBase::info(int argc, char *argv[]) {
-    if (argc == 0) {
-      chprintf(tty, "info     -- program/toolchain information\r\n");
-    } else {
-      chprintf(tty, "Kernel:       %s\r\n", CH_KERNEL_VERSION);
+void InfoCommand::exec(int argc, char *argv[]) {
+  if (argc == 0) {
+    chprintf(tty, "info     -- program/toolchain information\r\n");
+  } else {
+    chprintf(tty, "Kernel:       %s\r\n", CH_KERNEL_VERSION);
 #ifdef CH_COMPILER_NAME
-      chprintf(tty, "Compiler:     %s\r\n", CH_COMPILER_NAME);
+    chprintf(tty, "Compiler:     %s\r\n", CH_COMPILER_NAME);
 #endif
-      chprintf(tty, "Architecture: %s\r\n", CH_ARCHITECTURE_NAME);
+    chprintf(tty, "Architecture: %s\r\n", CH_ARCHITECTURE_NAME);
 #ifdef CH_CORE_VARIANT_NAME
-      chprintf(tty, "Core Variant: %s\r\n", CH_CORE_VARIANT_NAME);
+    chprintf(tty, "Core Variant: %s\r\n", CH_CORE_VARIANT_NAME);
 #endif
 #ifdef CH_PORT_INFO
-      chprintf(tty, "Port Info:    %s\r\n", CH_PORT_INFO);
+    chprintf(tty, "Port Info:    %s\r\n", CH_PORT_INFO);
 #endif
 #ifdef PLATFORM_NAME
-      chprintf(tty, "Platform:     %s\r\n", PLATFORM_NAME);
+    chprintf(tty, "Platform:     %s\r\n", PLATFORM_NAME);
 #endif
 #ifdef BOARD_NAME
-      chprintf(tty, "Board:        %s\r\n", BOARD_NAME);
+    chprintf(tty, "Board:        %s\r\n", BOARD_NAME);
 #endif
 #ifdef __DATE__
 #ifdef __TIME__
-      chprintf(tty, "Build time:   %s%s%s\r\n", __DATE__, " - ", __TIME__);
+    chprintf(tty, "Build time:   %s%s%s\r\n", __DATE__, " - ", __TIME__);
 #endif
 #endif
-    }
   }
-
-  extern "C" char __init_array_start;
-  extern "C" char _etext;
-  extern "C" char __main_stack_base__;
-  extern "C" char _data;
-  extern "C" char _edata;
-  extern "C" char _bss_start;
-  extern "C" char _bss_end;
-
-
-  void ShellBase::memory(int argc, char *argv[]) {
-    if (argc == 0) {
-      chprintf(tty, "memory   -- display RAM/ROM info\r\n");
-    } else {
-      size_t n, size;
-      n = chHeapStatus(NULL, &size);
-#define MEM(fmt,start,end) chprintf(tty, fmt, (start), (end), (1023 + ((end)-(start))) >> 10)
-      MEM(".text            : %.8x - %.8x (%dK)\r\n", &__init_array_start, &_etext);
-      MEM(".data            : %.8x - %.8x (%dK)\r\n", &_data, &_edata);
-      MEM(".bss             : %.8x - %.8x (%dK)\r\n", &_bss_start, &_bss_end);
-#undef MEM
-
-      chprintf(tty, "core free memory : %u bytes\r\n", chCoreStatus());
-      chprintf(tty, "heap fragments   : %u\r\n", n);
-      chprintf(tty, "heap free total  : %u bytes\r\n", size);
-    }
-  }
+}
 
 #if CH_USE_REGISTRY
-  extern "C" {
-    extern uint8_t __main_thread_stack_end__;
-  };
-
-  void ShellBase::threads(int argc, char *argv[]) {
-    static const char *states[] = {THD_STATE_NAMES};
-
-    if (argc == 0) {
-      chprintf(tty, "threads  -- list ChibiOS threads\r\n");
-    } else {
-      chprintf(tty, "%8s %4s %4s %4s %9s %8s %s\r\n",
-               "addr", "stku", "prio", "refs", "state", "time", "name");
-
-      for (::Thread *t=chRegFirstThread(); t; t = chRegNextThread(t)) {
-#if defined(CH_DBG_HAVE_STKTOP) && defined(CH_DBG_ENABLE_STACK_CHECK)
-        uint8_t *stack_top, *untouched;
-
-        if (t->p_stktop == 0) { // must be the main thread
-          stack_top = &__main_thread_stack_end__;
-        } else {
-          stack_top = ((uint8_t *) t->p_stktop) ;
-        }
-
-        untouched = (uint8_t *) t->p_stklimit;
-        while (*untouched == CH_STACK_FILL_VALUE && untouched < stack_top) ++untouched;
-
-        uint32_t stack_used = untouched - (uint8_t *) t->p_stklimit;
-#else
-        uint32_t stack_used = 0;
-#endif
-
-        chprintf(tty, "%.8lx %.4lx %4lu %4lu %9s %.8lu %s\r\n",
-                 (uint32_t)t, (uint32_t) stack_used /*t->p_ctx.r13*/,
-                 (uint32_t)t->p_prio, (uint32_t)(t->p_refs - 1),
-                 states[t->p_state], (uint32_t)t->p_time,
-                 chRegGetThreadName(t));
-      }
-    }
-  }
-#endif
-
-  void ShellBase::reset(int argc, char *argv[]) {
-    if (argc == 0) {
-      chprintf(tty, "reset    -- force hard reset\r\n");
-    } else {
-      asm volatile ("dsb");                                      /* Ensure all outstanding memory accesses included
-                                                                  buffered write are completed before reset */
-      SCB->AIRCR  = ((0x5FA << SCB_AIRCR_VECTKEY_Pos)      |
-                     (SCB->AIRCR & SCB_AIRCR_PRIGROUP_Msk) |
-                     SCB_AIRCR_SYSRESETREQ_Msk);                   /* Keep priority group unchanged */
-      asm volatile ("dsb");
-      while(1);                                                    /* wait until reset */
-    }
-  }
+extern "C" {
+  extern uint8_t __main_thread_stack_end__;
 };
+
+ThreadsCommand::ThreadsCommand() :
+  ShellCommand("threads")
+{
+}
+
+void ThreadsCommand::exec(int argc, char *argv[]) {
+  static const char *states[] = {THD_STATE_NAMES};
+
+  if (argc == 0) {
+    chprintf(tty, "threads  -- list ChibiOS threads\r\n");
+  } else {
+    chprintf(tty, "%8s %4s %4s %4s %9s %8s %s\r\n",
+             "addr", "stku", "prio", "refs", "state", "time", "name");
+
+    for (::Thread *t=chRegFirstThread(); t; t = chRegNextThread(t)) {
+#if defined(CH_DBG_HAVE_STKTOP) && defined(CH_DBG_ENABLE_STACK_CHECK)
+      uint8_t *stack_top, *untouched;
+
+      if (t->p_stktop == 0) { // must be the main thread
+        stack_top = &__main_thread_stack_end__;
+      } else {
+        stack_top = ((uint8_t *) t->p_stktop) ;
+      }
+
+      untouched = (uint8_t *) t->p_stklimit;
+      while (*untouched == CH_STACK_FILL_VALUE && untouched < stack_top) ++untouched;
+
+      uint32_t stack_used = untouched - (uint8_t *) t->p_stklimit;
+#else
+      uint32_t stack_used = 0;
+#endif
+
+      chprintf(tty, "%.8lx %.4lx %4lu %4lu %9s %.8lu %s\r\n",
+               (uint32_t)t, (uint32_t) stack_used /*t->p_ctx.r13*/,
+               (uint32_t)t->p_prio, (uint32_t)(t->p_refs - 1),
+               states[t->p_state], (uint32_t)t->p_time,
+               chRegGetThreadName(t));
+    }
+  }
+}
+#endif
+
+extern "C" char __init_array_start;
+extern "C" char _etext;
+extern "C" char __main_stack_base__;
+extern "C" char _data;
+extern "C" char _edata;
+extern "C" char _bss_start;
+extern "C" char _bss_end;
+
+MemoryCommand::MemoryCommand() :
+  ShellCommand("memory")
+{
+}
+
+void MemoryCommand::exec(int argc, char *argv[]) {
+  if (argc == 0) {
+    chprintf(tty, "memory   -- display RAM/ROM info\r\n");
+  } else {
+    size_t n, size;
+    n = chHeapStatus(NULL, &size);
+#define MEM(fmt,start,end) chprintf(tty, fmt, (start), (end), (1023 + ((end)-(start))) >> 10)
+    MEM(".text            : %.8x - %.8x (%dK)\r\n", &__init_array_start, &_etext);
+    MEM(".data            : %.8x - %.8x (%dK)\r\n", &_data, &_edata);
+    MEM(".bss             : %.8x - %.8x (%dK)\r\n", &_bss_start, &_bss_end);
+#undef MEM
+
+    chprintf(tty, "core free memory : %u bytes\r\n", chCoreStatus());
+    chprintf(tty, "heap fragments   : %u\r\n", n);
+    chprintf(tty, "heap free total  : %u bytes\r\n", size);
+  }
+}
+
+ResetCommand::ResetCommand() :
+  ShellCommand("reset")
+{
+}
+
+void ResetCommand::exec(int argc, char *argv[]) {
+  if (argc == 0) {
+    chprintf(tty, "reset    -- force hard reset\r\n");
+  } else {
+    // Ensure all outstanding memory accesses included
+    // buffered write are completed before reset
+    asm volatile ("dsb");
+
+    // Keep priority group unchanged
+    SCB->AIRCR  = ((0x5FA << SCB_AIRCR_VECTKEY_Pos)      |
+                   (SCB->AIRCR & SCB_AIRCR_PRIGROUP_Msk) |
+                   SCB_AIRCR_SYSRESETREQ_Msk);
+    asm volatile ("dsb");
+
+    // wait until reset
+    while(1);
+  }
+}
--- akt/shell.h
+++ akt/shell.h
@@ -2,51 +2,178 @@
 
 #pragma once
 
-#include "akt/thread.h"
+#include "akt/ring.h"
+
+#include "ch.h"
+#include "hal.h"
 
 namespace akt {
-  class ShellBase : public ThreadBase {
+  /**
+   * @class ShellCommand
+   * @brief Parses lines read from a serial console and executes commands
+   *
+   * This class implements a simple command line shell. Input lines are parsed
+   * into words separated by whitespace. The first word on each line names the
+   * command to be executed.
+   *
+   * Each command is represented by a single instance of a ShellCommand
+   * subclass, usually declared statically. ShellCommand's constructor
+   * automatically adds each instance to a global list of known commands.
+   *
+   * Applications using a shell should first initialize the tty variable
+   * to point at a valid BaseSequentialStream. Then the run_loop() method
+   * should be called in a dedicated thread.
+   */
+  class ShellCommand : public Ring<ShellCommand> {
+  public:
+    /**
+     * @brief Constructs an instance and adds it to the list of all commands
+     * @param[in] string which will invoke this command from the tty
+     */
+    ShellCommand(const char *name);
+
+    /**
+     * @brief Executes a specific command with pre-parsed args
+     * @param[in] argc the number of arguments (>= 0)
+     * @param[in] argv[] an array of null terminated argument strings
+     * 
+     * When argc == 0, the command should print out help information
+     * to the tty
+     */
+    virtual void exec(int argc, char *argv[]) = 0;
+
+    /**
+     * @brief Repeatedly reads a line from the console and executes the command
+     * @details Under normal circumstances, this function never returns
+     */
+    static msg_t run_loop(void *);
+
+    static BaseSequentialStream *tty;   /// serial console used by shell commands
+    static const char *prompt;          /// string printed out before a line is read from the tty
+
   protected:
+    /**
+     * @brief Tries to locate a Command instance with the specified name
+     * @param[in] name null terminated name
+     * @returns a pointer to the command with a matching name
+     * @returns 0 otherwise
+     */
+    static ShellCommand *find_command(const char *name);
+
     enum {
-      LINE_SIZE = 80,
-      MAX_ARGS = 5
+      LINE_SIZE = 80,                   /// size of internal line buffer
+      MAX_ARGS = 5                      /// max number of words parsed
     };
 
-    typedef struct {
-      void (*fn)(ShellBase *shell, int argc, char *argv[]);
-      const char *name;
-    } command_entry;
+    const char *name;                   /// string used to invoke this command from the tty
+    static void read_line();            /// reads a single line of text from the tty with minimal editing
+    static void parse_line();           /// parses a line of text into words separated by whitespace
 
-    SerialDriver *sd;
-    BaseSequentialStream *tty;
+    static Ring<ShellCommand> commands; /// linked list of all known commands
 
-    char line[LINE_SIZE];
-    unsigned int ll; // line length
-    char *argv[MAX_ARGS];
-    unsigned int argc;
-    const char *prompt;
+    static char line[LINE_SIZE];        /// line buffer for tty input
+    static unsigned int line_length;    /// number of chars in the line buffer
+    static char *argv[MAX_ARGS];        /// parsed words
+    static unsigned int argc;           /// number of parsed words
+  };
 
-    void readline();
-    void parseline();
-
-    void puts(const char *msg);
-    void list_commands();
-
-    const command_entry *find(const char *name);
-    const command_entry *command_table;
-    static const command_entry default_command_table[];
-
-    void info(int argc, char *argv[]);
-    void help(int argc, char *argv[]);
-    void threads(int argc, char *argv[]);
-    void memory(int argc, char *argv[]);
-    void reset(int argc, char *argv[]);
-
-    msg_t run();
-
+  /**
+   * @brief Prints out a list of known commands
+   * 
+   * Example output:
+   * > help                                        
+   * reset    -- force hard reset                  
+   * memory   -- display RAM/ROM info              
+   * threads  -- list ChibiOS threads              
+   * info     -- program/toolchain information     
+   * help     -- list available commands           
+   */
+  class HelpCommand : public ShellCommand {
   public:
-    ShellBase(SerialDriver *sd, const command_entry *commands,
-              void *wa, size_t wa_size,
-              const char *name, tprio_t prio);
+    HelpCommand();
+    void exec(int argc, char *argv[]) override;
+  };
+
+  /**
+   * @brief Prints out build info
+   *
+   * Example output:
+   * > info                                                                          
+   * Kernel:       2.7.0unstable                                                     
+   * Compiler:     GCC 4.7.3 20130312 (release) [ARM/embedded-4_7-branch revision 19]
+   * Architecture: ARMv7-ME                                                          
+   * Core Variant: Cortex-M4                                                         
+   * Port Info:    Advanced kernel mode                                              
+   * Platform:     STM32F407/F417 High Performance with DSP and FPU                  
+   * Board:        Hannibal v2 2013-11-22                                            
+   * Build time:   Jan  5 2014 - 11:05:02                                            
+   */
+  class InfoCommand : public ShellCommand {
+  public:
+    InfoCommand();
+    void exec(int argc, char *argv[]) override;
+  };
+
+#if CH_USE_REGISTRY
+  /**
+   * @brief Lists ChibiOS/RT threads
+   *
+   * Example output:
+   * 
+   * > threads                                               
+   *     addr stku prio refs     state     time name         
+   * 200012B8 0000   64    1  SLEEPING 00000342 main         
+   * 200011F8 0000    1    1     READY 00006109 idle         
+   * 20001718 0000    2    1     READY 00000000 usb_lld_pump 
+   * 20002200 0000   64    1   CURRENT 00000001 shell        
+   * 20002048 0000   64    1  SLEEPING 00000000 buttons      
+   */
+  class ThreadsCommand : public ShellCommand {
+  public:
+    ThreadsCommand();
+    void exec(int argc, char *argv[]) override;
+  };
+#endif
+
+  /**
+   * @brief Prints out memory allocation/usage
+   *
+   * Example output:
+   * 
+   * > memory                                         
+   * .text            : 080001AC - 0802A758 (170K)    
+   * .data            : 20000800 - 200011F4 (3K)      
+   * .bss             : 200011F8 - 200032A0 (9K)      
+   * core free memory : 101728 bytes                  
+   * heap fragments   : 0                             
+   * heap free total  : 0 bytes                       
+   */
+  class MemoryCommand : public ShellCommand {
+  public:
+    MemoryCommand();
+    void exec(int argc, char *argv[]) override;
+  };
+
+  /**
+   * @brief Forces a software reset of the system
+   *
+   */
+  class ResetCommand : public ShellCommand {
+  public:
+    ResetCommand();
+    void exec(int argc, char *argv[]) override;
+  };
+
+  /**
+    @brief Creates all the built-in shell commands with one declaration
+  */
+  struct BuiltInShellCommands {
+    HelpCommand help_command;
+    InfoCommand info_command;
+#if CH_USE_REGISTRY
+    ThreadsCommand threads_command;
+#endif
+    MemoryCommand memory_command;
+    ResetCommand reset_command;
   };
 };
