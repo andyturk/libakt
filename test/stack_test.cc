1020b20cb547d5f6c18e51643512ac0485f27445
Andy Turk <andyturk@gmail.com>
2014-03-19 08:28:09 -0700

    test depth() method

--- test/stack_test.cc
+++ test/stack_test.cc
@@ -25,11 +25,13 @@ protected:
 };
 
 TEST_F(StackTest, TestEmpty) {
+  EXPECT_TRUE(empty_stack.depth() == 0);
   EXPECT_TRUE(empty_stack.empty());
   EXPECT_FALSE(data_stack.empty());
 }
 
 TEST_F(StackTest, TestFull) {
+  EXPECT_TRUE(full_stack.depth() == 3);
   EXPECT_FALSE(empty_stack.full());
   EXPECT_TRUE(full_stack.full());
 }
@@ -38,10 +40,12 @@ TEST_F(StackTest, TestReset) {
   EXPECT_TRUE(empty_stack.empty());
   empty_stack.reset();
   EXPECT_TRUE(empty_stack.empty());
+  EXPECT_TRUE(empty_stack.depth() == 0);
 
   EXPECT_FALSE(data_stack.empty());
   data_stack.reset();
   EXPECT_TRUE(data_stack.empty());
+  EXPECT_TRUE(data_stack.depth() == 0);
 }
 
 TEST_F(StackTest, TestSpaceRemaining) {
@@ -53,12 +57,16 @@ TEST_F(StackTest, TestSpaceRemaining) {
 TEST_F(StackTest, TestPushPop) {
   int dummy;
 
+  EXPECT_TRUE(data_stack.depth() == 3);
   ASSERT_TRUE(data_stack.pop(dummy));
   ASSERT_EQ(789, dummy);
+  EXPECT_TRUE(data_stack.depth() == 2);
   ASSERT_TRUE(data_stack.pop(dummy));
   ASSERT_EQ(456, dummy);
+  EXPECT_TRUE(data_stack.depth() == 1);
   ASSERT_TRUE(data_stack.pop(dummy));
   ASSERT_EQ(123, dummy);
+  EXPECT_TRUE(data_stack.depth() == 0);
 
   ASSERT_TRUE(data_stack.empty());
   ASSERT_EQ(5, data_stack.space_remaining());
